ea3ac236c64461c2963a39d5d430c21b8490fead
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-05-07 18:23:02 +0200

    Added form

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!-- HTML -->
 <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -7,6 +8,18 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
-        
+        <main>
+            <!-- FORM -->
+            <section>
+                <div class="container">
+                    <div class="my-5 text-center">
+                        <form action="">
+                            <input type="email" name="mail" placeholder="Inserire la propria email">
+                            <button>Invia</button>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </main>
     </body>
 </html>
