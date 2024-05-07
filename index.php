063fb64b000ebb2c05c5f492117a5b12b4b97192
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-05-07 23:33:02 +0200

    added header and footer

--- index.php
+++ index.php
@@ -12,23 +12,34 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
-        <main>
-            <section>
-                <div class="container">
-                    <div class="my-5 text-center">
-                        <!-- FORM -->
-                        <form action="" method="post">
-                            <input type="email" name="mail" placeholder="Inserire la propria email">
-                            <button>Invia</button>    
-                        </form>
-                        <!-- ALERT -->
-                        <div class="alert alert-primary my-5" role="alert">
-                            <?php echo mail_ceck($user_mail) === true ? "Email valida" : "Inserire Email valida" ; ?>
-                        </div>                       
+        <div class="vh-100 d-flex flex-column" id="app">
+            <!-- HEADER -->
+            <?php
+            include __DIR__ . '/partials/templates/header.php';
+            ?>
+            <!-- MAIN -->
+            <main class="flex-grow-1">
+                <section>
+                    <div class="container">
+                        <div class="my-5 text-center">
+                            <!-- FORM -->
+                            <form action="" method="post">
+                                <input type="email" name="mail" placeholder="Inserire la propria email">
+                                <button>Invia</button>    
+                            </form>
+                            <!-- ALERT -->
+                            <div class="alert alert-primary my-5" role="alert">
+                                <?php echo mail_ceck($user_mail) === true ? "Email valida" : "Inserire Email valida" ; ?>
+                            </div>                       
+                        </div>
                     </div>
-                </div>
-            </section>
-        </main>
+                </section>
+            </main>
+            <!-- FOOTER -->
+            <?php 
+            include __DIR__ . '/partials/templates/footer.php';
+            ?>
+        </div>
 
         <!-- BOOTSTRAP JS -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
---
+++ partials/templates/header.php
@@ -0,0 +1,13 @@
+<header class="py-3">
+    <div class="container">
+        <div class="text-center">
+            <h1>INSERISCI LA TUA MAIL</h1>
+        </div>
+    </div>
+</header>
+
+<style>
+    header{
+        background-color: lightblue;
+    }
+</style>
