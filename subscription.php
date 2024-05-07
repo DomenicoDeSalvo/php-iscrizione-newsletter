d16f5c3bc6b5ba39ec1d5c9074f9b79629e1fab1
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-05-08 01:49:17 +0200

    Added heaer and footer to subscription.php

--- subscription.php
+++ subscription.php
@@ -6,7 +6,7 @@ $value = $_SESSION["value"] ?? '';
 ?>
 
 <!DOCTYPE html>
-    <html lang="en">
+<html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,11 +14,24 @@ $value = $_SESSION["value"] ?? '';
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
-        <div class="container">
-            <!-- ALERT -->
-            <div class="alert text-center alert-primary my-5" role="alert">
-                <?php echo $value === true ? "Email valida" : "Inserire Email valida" ; ?>
-            </div>
+    <div class="vh-100 d-flex flex-column" id="app">
+            <!-- HEADER -->
+            <?php
+            include __DIR__ . '/partials/templates/header.php';
+            ?>
+            <!-- MAIN -->
+            <main class="flex-grow-1">
+                <div class="container">
+                    <!-- ALERT -->
+                    <div class="alert text-center alert-primary my-5" role="alert">
+                        <?php echo $value === true ? "Email valida" : "Inserire Email valida" ; ?>
+                    </div>
+                </div>  
+            </main>
+            <!-- FOOTER -->
+            <?php 
+            include __DIR__ . '/partials/templates/footer.php';
+            ?>
         </div>
     </body>
 </html>
